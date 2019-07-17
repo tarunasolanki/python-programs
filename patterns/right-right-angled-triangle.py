f26b4a1e301a41b6dd42f37c6229a3d02080929e
unknown <tarunasolanki03@gmail.com>
2019-07-17 17:41:39 +0530

    Initial init, adding all files first time to the repository

---
+++ patterns/right-right-angled-triangle.py
@@ -0,0 +1,13 @@
+#
+#Program to print star pattern 
+#    *
+#   **
+#  ***
+# ****
+#*****
+for i in range(1,6):
+    for j in range (i,i,-1):
+        print(" ",end="")
+    for k in range (1,i,1):
+        print("*",end="")
+    print("")
